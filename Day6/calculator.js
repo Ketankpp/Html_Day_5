818902f4977d26f273ccb41a7fe51f664ef2d418
Ketankpp <ketanpradippatil14@gmail.com>
2023-05-14 22:24:36 +0530

    ok

---
+++ Day6/calculator.js
@@ -0,0 +1,12 @@
+// This function clear all the values
+function clearScreen() {
+    document.getElementById("result").value = "";
+    }
+    function display(value) {
+    document.getElementById("result").value += value;
+    }
+    function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+    }
